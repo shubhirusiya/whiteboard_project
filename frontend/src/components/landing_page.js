11167571294ecd6794949677cf199b705659d6d1
sudeepti25 <sudeepti2006@gmail.com>
2024-12-11 20:51:38 +0530

    landing page continued

--- frontend/src/components/landing_page.js
+++ frontend/src/components/landing_page.js
@@ -7,7 +7,7 @@ import MouseTrail from "./mousetraileffect";
 function landingpage() {
   return (
     
-    <div style={{ height: "100vh", backgroundColor: "white", overflow:"hidden" }}>
+    <div style={{ minHeight:"100vh", backgroundColor: "white", overflow:"hidden" }}>
      
         <div className='intro'>
           <div className="textarea">
@@ -27,6 +27,38 @@ function landingpage() {
 
 
         </div>
+        <div className='intro'>
+        <div className='pic'><img src="./doodle.png" alt="" /></div>
+
+          <div className="textarea">
+            <h1>Engage everyone</h1>
+            <br/>
+          <p>
+          Collab with anyone, anywhere and start working 
+            </p>
+            <br />
+      
+          </div>
+
+
+        </div>
+
+        <div className='intro'>
+        <div className='pic1'><img src="./note1.png" alt="" /></div>
+
+          <div className="textarea">
+            <h1>Study Together,Learn Together</h1>
+            <br/>
+          <p>
+          Want to study and learn together ? Try the whiteboard now !
+            </p>
+            <br />
+      
+          </div>
+
+
+        </div>
+       
         <MouseTrail />
     </div>
   );
